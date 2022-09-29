d8d5af278cbdafd9efa62fdf524b8f57852a538f
Abrahamcepedao <a00827666@itesm.mx>
2022-09-29 16:48:44 -0400

    Updated recetas page, dynamic

---
+++ data/recipes.tsx
@@ -0,0 +1,84 @@
+const recipes = [
+    {
+        id: 1,
+        type: 'Desayunos',
+        title: "Hot cakes de avena",
+        subtitle: "Hot cakes super sanos y deliciosos",
+        img: "hotcakes.jpg",
+        ingredients: [
+            "Crema de cacahuate",
+            "1/2 taza de hojuelas de avena",
+            "1 taza de harina de avena",
+            "1 cucharadita de royal",
+            "Miel maple",
+            "1 taza de leche de almendra o soya",
+            "1 cucharadita de aceite de coco",
+            "1 1/2 tazas de harina de amaranto",
+            "2 huevos de linaza",
+            "3 plátanos maduros",
+            "2 cucharadas de azúcar mascabado",
+        ],
+        proceso: [
+            "Licuar todos los ingredientes, cocina y disfruta con miel maple o crema de cacahuate"
+        ],
+        notas: [
+            "Huevos de linaza: mezcla 1 cucharada de linaza con 3 cucharadas de agua, revuelve y deja reposar por unos 10 minutos."
+        ],
+    },
+    {
+        id: 2,
+        type: 'Snacks',
+        title: "Manzana decorada",
+        subtitle: "Delicioso snack",
+        img: "manzana.jpg",
+        ingredients: [
+            "Manzana",
+            "Crema de cacahuate Original",
+            "Nuez de macadamia",
+            "Coco rallado",
+            "Chispas de chocolate sin leche",
+        ],
+        proceso: [
+            "Corta la manzana en rodajas", 
+            "Una la crema de cacahuate",
+            "Encima decora con los demás ingredientes"
+        ],
+        notas: []
+    },
+    {
+        id: 3,
+        type: 'Snacks',
+        title: "Manzana con crema de cacahuate",
+        subtitle: "Snack con energía",
+        img: "manzana.jpg",
+        ingredients: [
+            "Manzana",
+            "Crema de cacahuate Original",
+        ],
+        proceso: [
+            "Corta la manzana en rodajas",
+            "Coloca la crema de cacahuate en un bowl pequeño",
+        ],
+        notas: [
+            " Este puede ser un snack o también perfecto como postre"
+        ]
+    },
+    {
+        id: 4,
+        type: 'Postres',
+        title: "Pan de muerto con crema de cacahuate",
+        subtitle: "Has probado esta combinación?",
+        img: "pan.jpg",
+        ingredients: [
+            "Crema de cachuate Original",
+            "Pan de muerto",
+            "Plátano",
+        ],
+        proceso: [
+            "Parte el pan de muerto a la mitad, unta crema de cacahuate y encima pon plátano rebanado"
+        ],
+        notas: []
+    }
+]
+
+export { recipes };
